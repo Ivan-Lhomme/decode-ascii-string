357423d36a6b2de5caa47446bf25c2ecb1b0edc2
Ivan <i.lhommeageorges@gmail.com>
2026-04-21 11:52:14 +0200

    Finish decoder

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,24 @@ use std::io;
 
 fn main() {
     let mut encode_str = String::new();
-
+    let mut bytes = Vec::new();
+    
     println!("Put the encode string :");
     io::stdin().read_line(&mut encode_str).expect("Failed to read line.");
 
-    println!("{}", encode_str)
+    let encode_str = encode_str.trim();
+    
+    let mut i = 0;
+    while i < encode_str.len() {
+        let encode_char = &encode_str[i..i + 2];
+        let byte = u8::from_str_radix(encode_char, 16).unwrap();
+
+        bytes.push(byte);
+
+        i += 2;
+    }
+    
+    let decode_str = String::from_utf8(bytes).unwrap();
+
+    println!("{}", decode_str)
 }
